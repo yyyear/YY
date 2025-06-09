dae779f367204157a6d8d2a95bb2362712e3b735
zhangyaju <aeagle@qq.com>
2025-06-09 15:52:48 +0800

    interface to string

--- YYKit.go
+++ YYKit.go
@@ -131,3 +131,50 @@ func MD5Upper(s string) string {
 func MD5Lower(s string) string {
 	return strings.ToLower(MD5(s))
 }
+
+// InterfaceToString 将interface{}转换为string
+func InterfaceToString(i interface{}) string {
+	if i == nil {
+		return ""
+	}
+
+	switch v := i.(type) {
+	case string:
+		return v
+	case []byte:
+		return string(v)
+	case int:
+		return strconv.Itoa(v)
+	case int8:
+		return strconv.FormatInt(int64(v), 10)
+	case int16:
+		return strconv.FormatInt(int64(v), 10)
+	case int32:
+		return strconv.FormatInt(int64(v), 10)
+	case int64:
+		return strconv.FormatInt(v, 10)
+	case uint:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint8:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint16:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint32:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint64:
+		return strconv.FormatUint(v, 10)
+	case float32:
+		return strconv.FormatFloat(float64(v), 'f', -1, 32)
+	case float64:
+		return strconv.FormatFloat(v, 'f', -1, 64)
+	case bool:
+		return strconv.FormatBool(v)
+	default:
+		return fmt.Sprintf("%v", v)
+	}
+}
+
+// ToString 简化版本的interface{}转string
+func ToString(i interface{}) string {
+	return InterfaceToString(i)
+}
