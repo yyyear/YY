c976ed20e598b3f5711436fcbfd86891e627e736
aeagle <aeagle@qq.com>
2026-01-04 22:29:19 +0800

    string1.3.9

--- StringPool.go
+++ StringPool.go
@@ -9,30 +9,12 @@ var builderPool sync.Pool
 
 func init() {
 	builderPool = sync.Pool{New: func() interface{} {
-		return Build{}
+		return &strings.Builder{}
 	}}
 }
 
-type Build strings.Builder
-
-func NewBuilder() Build {
-	return builderPool.Get().(Build)
-}
-
-// Add 增加字符
-func (b Build) Add(r string) Build {
-	var j strings.Builder = strings.Builder(b)
-	j.WriteString(r)
-	return Build(j)
-}
-func (b Build) String() string {
-	builder := strings.Builder(b)
-	return builder.String()
-}
-func (b Build) Release() {
-	builder := strings.Builder(b)
-	builder.Reset()
-	builderPool.Put(builder)
+func NewBuilder() *strings.Builder {
+	return builderPool.Get().(*strings.Builder)
 }
 
 func BuilderRelease(b *strings.Builder) {
