f2422c28d13579363bfa63acc4d0c9025226ec93
aeagle <aeagle@qq.com>
2026-01-04 22:22:06 +0800

    builder

--- StringPool.go
+++ StringPool.go
@@ -9,15 +9,27 @@ var builderPool sync.Pool
 
 func init() {
 	builderPool = sync.Pool{New: func() interface{} {
-		return &strings.Builder{}
+		return Build{}
 	}}
 }
 
-type NSString string
+type Build strings.Builder
 
-// Builder 增加字符
-func Builder() *strings.Builder {
-	return builderPool.Get().(*strings.Builder)
+func NewBuilder() Build {
+	return Build(builderPool.Get().(strings.Builder))
+}
+
+// Add 增加字符
+func (b Build) Add(r string) Build {
+	var j strings.Builder = strings.Builder(b)
+	j.WriteString(r)
+	return Build(j)
+}
+
+func (b Build) Release() {
+	builder := strings.Builder(b)
+	builder.Reset()
+	builderPool.Put(builder)
 }
 
 func BuilderRelease(b *strings.Builder) {
